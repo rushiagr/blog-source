fc1759343bc61cdc69b1a6082bd4d7e0580c9ef0
Rushi Agrawal <rushi.agr@gmail.com>
2014-07-07 23:25:19 +0530

    Add basic pelicanconf.py
    octopress-style URLs, github, google analytics, disqus, twitter details mentioned.

---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+SITEURL = 'http://rushiagr.github.io'
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+GITHUB_USER = 'rushiagr'
+GITHUB_REPO_COUNT = 5
+GITHUB_SKIP_FORK = True
+GITHUB_SHOW_USER_LINK = True
+
+GOOGLE_ANALYTICS = 'UA-37074962-1'
+
+DISQUS_SITENAME = 'rushiagr'
+
+SEARCHBOX = True
+
+TWITTER_USER = 'reeshi_india'
+TWITTER_TWEET_BUTTON = True
+GOOGLE_PLUS_ONE = True
+FACEBOOK_LIKE = True
